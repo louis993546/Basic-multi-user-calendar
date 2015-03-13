fdea541432ac2e3e8dd999ffb8a5ed3fdd3b0641
Louis Tsai <louis993546@gmail.com>
2015-03-13 01:32:33 -0700

    Partial implementation of ApptDB
    1. New class Appointment have been created
    2. get function in ApptDB implemented, but have not been tested yet
    3. NewUserDialog and ModifyUserDialog created for phrase 2
    4. Reset Eclipse for Windows.

---
+++ calendar/src/hkust/cse/calendar/unit/Appointment.java
@@ -0,0 +1,90 @@
+package hkust.cse.calendar.unit;
+
+public class Appointment implements Comparable<Appointment> {
+	private String title;
+	private String description;
+	private String location;
+	private int startHour;
+	private int startMin;
+	private int startYear;
+	private int startMonth;
+	private int startDay;
+	private int endHour;
+	private int endMin;
+	private int endYear;
+	private int endMonth;
+	private int endDay;
+	private int reminder;
+	private int reminderTime;
+	private int reminderUnit;
+
+	public Appointment(String t, String d, String l, int shr, int smin, int syr, int smon, int sday, int ehr, int emin, int eyr, int emon, int eday, int r, int rt, int ru) {
+		title = t;
+		description = d;
+		location = l;
+		startHour = shr;
+		startMin = smin;
+		startYear = syr;
+		startMonth = smon;
+		startDay = sday;
+		endHour = ehr;
+		endMin = emin;
+		endYear = eyr;
+		endMonth = emon;
+		endDay = eday;
+		reminder = r;
+		reminderTime = rt;
+		reminderUnit = ru;
+	}
+	
+	public Appointment() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void setAppointment(String t, String d, String l, int shr, int smin, int syr, int smon, int sday, int ehr, int emin, int eyr, int emon, int eday, int r, int rt, int ru) {
+		title = t;
+		description = d;
+		location = l;
+		startHour = shr;
+		startMin = smin;
+		startYear = syr;
+		startMonth = smon;
+		startDay = sday;
+		endHour = ehr;
+		endMin = emin;
+		endYear = eyr;
+		endMonth = emon;
+		endDay = eday;
+		reminder = r;
+		reminderTime = rt;
+		reminderUnit = ru;
+	}
+
+	public boolean setStartDateTime(int shr, int smin, int syr, int smon, int sday) {
+		return false;
+		//check if date is valid
+		//check if time is valid
+	}
+	
+	public boolean setEndDateTime(int ehr, int emin, int eyr, int emon, int eday) {
+		return false;
+		//check if date is valid
+		//check if time is valid
+	}
+	
+	public boolean isDateValid(int yr, int mon, int day) {
+		return false;
+	}
+	
+	public boolean isTimeValid(int hr, int min) {
+		if (hr >= 0 && hr <= 24 && min >= 0 && min <= 60)
+			return true;
+		return false;
+	}
+	
+	@Override
+	public int compareTo(Appointment a) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
