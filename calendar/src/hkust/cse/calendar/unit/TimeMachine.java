fb4e6664bc3441227089bb2d44c19a996184c995
kwafok <kwafok@stu.ust.hk>
2015-04-02 14:31:01 +0800

    update 2

--- calendar/src/hkust/cse/calendar/unit/TimeMachine.java
+++ calendar/src/hkust/cse/calendar/unit/TimeMachine.java
@@ -18,30 +18,62 @@ import hkust.cse.calendar.apptstorage.ApptStorageNullImpl;
 import hkust.cse.calendar.gui.CalGrid;
 import hkust.cse.calendar.unit.TimeSpan;
 
-public class TimeMachine {
-	private static TimeMachine mTimeMachine = new TimeMachine();
-	private GregorianCalendar mToday;
+import java.sql.Timestamp;
+import java.util.Date;
 
-	
-	public static TimeMachine getInstance() {
-		return mTimeMachine;
+
+
+public class TimeMachine {
+	//private GregorianCalendar mToday;
+	//private Date timeAndDate;
+	private long offset; // unit ms
+	private CalGrid cg;// 
+	public TimeMachine(CalGrid cg) {
+		offset=0;
+		this.cg=cg;
 	}
 	
-	public TimeMachine() {
-		mToday = new GregorianCalendar();
+	public Timestamp getTMTimestamp() {
+		java.util.Date date= new java.util.Date();
+		long now=date.getTime();
+		//now+offset -> timestamp
+		return new Timestamp(offset+now);
+		
 		
 	}
 	
-	public Timestamp getMTimestamp() {
-		return new Timestamp(mToday.getTimeInMillis());
-	}
-	
-	public GregorianCalendar getMToday() {
-		return mToday;
-	}
-	
 	public void setTimeMachine(int year, int month, int day, int hour, int minute, int second) {
-		mToday.set(year, month, day, hour, minute, second);
-//		System.out.println(month);
+		//mToday.set(year, month, day, hour, minute, second){;
+		
+		//yyymmddhhmm -> timestamp -> gettime
+		Timestamp end = new Timestamp(0);
+		end.setYear(year-1900);
+		end.setMonth(month-1);
+		end.setDate(day);
+		end.setHours(hour);
+		end.setMinutes(minute);
+		end.setSeconds(second);
+		long endtime=end.getTime();
+		
+		//date() -> gettime
+		java.util.Date date= new java.util.Date();
+		long now=date.getTime();
+		
+		offset=endtime-now;
+		
+		this.cg.today.setTime(this.getTMTimestamp());
+		this.cg.UpdateCal();
+
+		
 	}
+//	public String toString(){
+//		return "Time of TM : " + this.getTMTimestamp().getYear()+ "/"+
+//				(this.getTMTimestamp().getMonth()+1) +"/"+
+//				this.getTMTimestamp().getDay()+" "+
+//				this.getTMTimestamp().getHours()+":"+
+//				this.getTMTimestamp().getMinutes()+":"+
+//				this.getTMTimestamp().getSeconds();
+//	}
+//		System.out.println(month);
+	
 }
