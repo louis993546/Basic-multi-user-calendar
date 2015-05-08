1a16076096099066ddf66a911f1aa08d14149925
kwafok <kwafok@stu.ust.hk>
2015-05-08 17:43:36 +0800

    more for message storage

--- calendar/src/hkust/cse/calendar/apptstorage/MessageStorage.java
+++ calendar/src/hkust/cse/calendar/apptstorage/MessageStorage.java
@@ -7,7 +7,11 @@ import hkust.cse.calendar.unit.MessageBody;
 import hkust.cse.calendar.unit.TimeMachine;
 import hkust.cse.calendar.unit.TimeSpan;
 
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,63 +20,69 @@ import java.util.TreeMap;
 
 public class MessageStorage {
 	static SortedMap<Integer, MessageBody> deleteUser = new TreeMap<Integer, MessageBody>();
-	//deleteUser.put(-1, new MessageBody(-1,-1,-1,MessageBody.UserResponse.NotYet,LocalDateTime.now());
+	// deleteUser.put(-1, new
+	// MessageBody(-1,-1,-1,MessageBody.UserResponse.NotYet,LocalDateTime.now());
 	SortedMap<Integer, MessageBody> deleteLocation = new TreeMap<Integer, MessageBody>();
 
-	private MessageStorage(){
-		
+	private MessageStorage() {
+
 	}
-	
-	public static SortedMap<Integer, MessageBody> getDeleteUser(){
+
+	public static SortedMap<Integer, MessageBody> getDeleteUser() {
 		return deleteUser;
 	}
-	
-	public SortedMap<Integer, MessageBody> getDeleteLocation(){
+
+	public SortedMap<Integer, MessageBody> getDeleteLocation() {
 		return deleteLocation;
 	}
 
 	public static void popupMsgAndSave(int i, String userOrLocation) {
 		MessageBody tmpMB = deleteUser.get(i);
-		System.out.println("user with id "+tmpMB.getUserToBeDeletedID()+ "will be deleted. Do you accept?");
-		AcceptOrNotDialog tmpDialog=new AcceptOrNotDialog();
-		//if yes, if no
-		
-		
+		System.out.println("user with id " + tmpMB.getUserToBeDeletedID()
+				+ "will be deleted. Do you accept?");
+		AcceptOrNotDialog tmpDialog = new AcceptOrNotDialog();
+		// if yes, if no
+
 	}
 
 	public static SortedMap<Integer, LocalDateTime> getCreatorToLastRelatedEventMap_notfinish(
 			int IDofUserToBeDeleted) {
 		// TODO Auto-generated method stub
 		// locked the user
-		SortedMap<Integer, LocalDateTime> tmpmap=new TreeMap<Integer, LocalDateTime>();
-		
-		
-		//appt[] apptlist=getApptListInvolveUserInFuture(uid);
-//		Appt[] apptlist=getApptListInvolveUserInFuture(IDofUserToBeDeleted);
-//		
-//		Map<Integer, List<LocalDateTime>> CreatorToEndTimesMap=new HashMap<Integer, List<LocalDateTime>>();
-//		for(Appt tmpappt: apptlist){
-//			tmpappt.getAppointment().getCreaterUID();
-//			TimeSpan tmpTimeSpan = tmpappt.getAppointment().getTimeSpan();
-//			
-//			
-//		}
-//		
-//		
-//        if(!hashMap.containsKey(locationId)){
-//            List<Location> list= new ArrayList<Location>();
-//            list.add(student);
-//        hashMap.put(locationId,list);
-//        }
-//        else
-//            hashMap.get(locationId).add(student);
-//		
-//		
-		LocalDateTime tmptime = TimeMachine.getInstance().getTMTimestamp().toLocalDateTime().plusHours(6);
-		tmpmap.put(IDofUserToBeDeleted, tmptime);
-		return tmpmap;
-	}
-	
-	
-}
+		SortedMap<Integer, LocalDateTime> tmpmap = new TreeMap<Integer, LocalDateTime>();
 
+		// appt[] apptlist=getApptListInvolveUserInFuture(uid);
+
+		Map<Integer, List<LocalDateTime>> CreatorToEndTimesMap = new HashMap<Integer, List<LocalDateTime>>();
+		for (Appt tmpappt : apptlist) {
+			int tmpUserID = tmpappt.getAppointment().getCreaterUID();
+			LocalDateTime endTime = tmpappt.getAppointment().getTimeSpan()
+					.EndTime().toLocalDateTime();
+			List<LocalDateTime> tmplist = new ArrayList<LocalDateTime>();// default
+																			// empty
+			if (CreatorToEndTimesMap.containsKey(tmpUserID)) {
+				tmplist = CreatorToEndTimesMap.get(tmpUserID);
+
+			}
+			tmplist.add(endTime);
+			CreatorToEndTimesMap.put(tmpUserID, tmplist);
+		}
+		// now CreatorToEndTimesMap is group by creator
+		// try get max endtime for diff creator
+
+		for (int i : CreatorToEndTimesMap.keySet()) {
+			List<LocalDateTime> listOfDateTime = CreatorToEndTimesMap.get(i);
+			LocalDateTime tmpmax = Collections.max(listOfDateTime, new Comparator<LocalDateTime>() {
+				public int compare(LocalDateTime o1, LocalDateTime o2) {
+					return o1.compareTo(o2);
+				}
+			});
+			CreatorToMaxEndTimeMap.put(i, tmpmax);
+
+		}
+		//now we got max time
+
+		return CreatorToMaxEndTimeMap;
+	}
+
+}
